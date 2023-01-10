55b1a48db175348ff63a1a9f3c70b7ec0a3aedd2
Chitral Verma <chitralverma@gmail.com>
2023-01-10 17:43:36 +0530

    Allow reads (#5)
    * Rust implementation of concat_df
    
    * Add reads for all formats supported by scan

--- native/src/internal_jni/frame.rs
+++ native/src/internal_jni/frame.rs
@@ -1,7 +1,15 @@
+#![allow(non_snake_case)]
+
 use jni::objects::JObject;
-use jni::sys::jlong;
+use jni::objects::ReleaseMode::NoCopyBack;
+use jni::sys::{jlong, jlongArray};
 use jni::JNIEnv;
 
+use polars::export::num::ToPrimitive;
+use polars::prelude::*;
+use polars_core::utils::concat_df;
+
+use crate::internal_jni::utils::*;
 use crate::j_data_frame::JDataFrame;
 
 #[no_mangle]
@@ -24,4 +32,27 @@ pub unsafe extern "system" fn Java_org_polars_scala_polars_internal_jni_data_1fr
     let count = j_df.df.shape().0 as i64;
 
     jlong::from(count)
+}
+
+#[no_mangle]
+pub unsafe extern "system" fn Java_org_polars_scala_polars_internal_jni_common_00024__1concatDataFrames(
+    env: JNIEnv,
+    object: JObject,
+    inputs: jlongArray,
+) -> jlong {
+    let arr = env.get_long_array_elements(inputs, NoCopyBack).unwrap();
+
+    let vec: Vec<DataFrame> =
+        std::slice::from_raw_parts(arr.as_ptr(), arr.size().unwrap() as usize)
+            .to_vec()
+            .iter()
+            .map(|p| p.to_i64().unwrap())
+            .map(|ptr| {
+                let j_ldf = &mut *(ptr as *mut JDataFrame);
+                j_ldf.to_owned().df
+            })
+            .collect();
+
+    let concat_ldf = concat_df(vec.as_slice());
+    df_to_ptr(env, object, concat_ldf)
 }
