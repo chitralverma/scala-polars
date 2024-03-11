394df9a49a30c85082c914f47f35d88b9d209ca0
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-03-11 08:32:05 +0100

    Update scala3-library to 3.3.3 (#80)

--- project/GeneralSettings.scala
+++ project/GeneralSettings.scala
@@ -8,7 +8,7 @@ object GeneralSettings {
 
   val scala212 = "2.12.19"
   val scala213 = "2.13.13"
-  val scala33 = "3.3.1"
+  val scala33 = "3.3.3"
 
   val defaultScalaVersion: String = scala33
   val supportedScalaVersions: Seq[String] = Seq(scala212, scala213, scala33)
