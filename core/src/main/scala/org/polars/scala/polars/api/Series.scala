103246e82ee3ed2d5977bbe622b00fe3f78f891e
Chitral Verma <chitralverma@gmail.com>
2024-03-19 18:40:29 +0530

    refactor series

--- core/src/main/scala/org/polars/scala/polars/api/Series.scala
+++ core/src/main/scala/org/polars/scala/polars/api/Series.scala
@@ -1,8 +1,5 @@
 package org.polars.scala.polars.api
 
-import scala.reflect.runtime.universe._
-
-import org.polars.scala.polars.api.types._
 import org.polars.scala.polars.internal.jni.series
 
 class Series private (private[polars] val ptr: Long) {
@@ -13,86 +10,62 @@ class Series private (private[polars] val ptr: Long) {
 object Series {
   private final val EmptyString = ""
 
-  def of[T: TypeTag](
-      name: String,
-      data: Array[T],
-      dtype: Option[DataType] = None
-  ): Series = {
-    val ptr = data.headOption match {
-      case Some(_: Int) => series.new_int_series(name, data.map(_.asInstanceOf[Int]))
-      case Some(_: Long) => series.new_long_series(name, data.map(_.asInstanceOf[Long]))
-      case Some(_: Float) => series.new_float_series(name, data.map(_.asInstanceOf[Float]))
-      case Some(_: Double) => series.new_double_series(name, data.map(_.asInstanceOf[Double]))
-      case Some(_: Boolean) => series.new_boolean_series(name, data.map(_.asInstanceOf[Boolean]))
-      case Some(_: Array[_]) =>
-        val internalPtrs = data.map {
-          case e: Array[Int] => Series.of(EmptyString, e).ptr
-          case e: Array[Long] => Series.of(EmptyString, e).ptr
-          case e: Array[Float] => Series.of(EmptyString, e).ptr
-          case e: Array[Double] => Series.of(EmptyString, e).ptr
-          case e: Array[Boolean] => Series.of(EmptyString, e).ptr
-          case e: Array[java.time.LocalDate] => Series.of(EmptyString, e).ptr
-          case e: Array[java.time.LocalDateTime] => Series.of(EmptyString, e).ptr
-          case e: Array[String] => Series.of(EmptyString, e).ptr
-          case _ =>
-            throw new IllegalArgumentException(
-              s"List Series of provided internal type is currently not supported."
-            )
-        }
-        series.new_list_series(name, internalPtrs)
-      case Some(_: java.time.LocalDate) =>
-        series.new_date_series(
-          name,
-          data.map(v =>
-            java.time.format.DateTimeFormatter.ISO_LOCAL_DATE
-              .format(v.asInstanceOf[java.time.LocalDate])
+  def of(name: String, data: Array[Series]): Series =
+    Series.withPtr(series.new_list_series(name, data.map(_.ptr)))
+
+  def of(name: String, data: Array[Int]): Series =
+    Series.withPtr(series.new_int_series(name, data))
+
+  def of(name: String, data: Array[Long]): Series =
+    Series.withPtr(series.new_long_series(name, data))
+
+  def of(name: String, data: Array[Float]): Series =
+    Series.withPtr(series.new_float_series(name, data))
+
+  def of(name: String, data: Array[Double]): Series =
+    Series.withPtr(series.new_double_series(name, data))
+
+  def of(name: String, data: Array[Boolean]): Series =
+    Series.withPtr(series.new_boolean_series(name, data))
+
+  def of(name: String, data: Array[String]): Series =
+    Series.withPtr(series.new_str_series(name, data))
+
+  def of(name: String, data: Array[java.time.LocalDate]): Series =
+    Series.withPtr(
+      series.new_date_series(
+        name,
+        data.map(v => java.time.format.DateTimeFormatter.ISO_LOCAL_DATE.format(v))
+      )
+    )
+
+  def of(name: String, data: Array[java.time.LocalDateTime]): Series =
+    Series.withPtr(
+      series.new_datetime_series(
+        name,
+        data.map(v => java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(v))
+      )
+    )
+
+  def of[T](name: String, data: Array[Array[T]]): Series =
+    Series.of(
+      name,
+      data.map {
+        case e: Array[Int] => Series.of(EmptyString, e)
+        case e: Array[Long] => Series.of(EmptyString, e)
+        case e: Array[Float] => Series.of(EmptyString, e)
+        case e: Array[Double] => Series.of(EmptyString, e)
+        case e: Array[Boolean] => Series.of(EmptyString, e)
+        case e: Array[java.time.LocalDate] => Series.of(EmptyString, e)
+        case e: Array[java.time.LocalDateTime] => Series.of(EmptyString, e)
+        case e: Array[String] => Series.of(EmptyString, e)
+        case e: Array[Array[_]] => Series.of(EmptyString, e)
+        case _ =>
+          throw new IllegalArgumentException(
+            s"List Series of provided type `${data.getClass.getSimpleName}` is currently not supported."
           )
-        )
-      case Some(_: java.time.LocalDateTime) =>
-        series.new_datetime_series(
-          name,
-          data.map(v =>
-            java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME
-              .format(v.asInstanceOf[java.time.LocalDateTime])
-          )
-        )
-      case Some(_: String) => series.new_str_series(name, data.map(_.asInstanceOf[String]))
-      case None =>
-        dtype match {
-          case Some(dt) => empty(name, dt).ptr
-          case None =>
-            throw new IllegalArgumentException(
-              "Empty `data` without `dtype` is not sufficient to form series. " +
-                "To form empty series either use `Series.empty(...)` or provide `dtype`.."
-            )
-        }
-      case _ =>
-        throw new IllegalArgumentException(
-          s"Series of data type `${typeOf[T].typeSymbol.name}` is currently not supported."
-        )
-    }
-
-    Series.withPtr(ptr)
-  }
-
-  def empty(name: String, dtype: DataType): Series = {
-    val ptr = dtype match {
-      case IntType => series.new_int_series(name, Array.empty[Int])
-      case LongType => series.new_long_series(name, Array.empty[Long])
-      case FloatType => series.new_float_series(name, Array.empty[Float])
-      case DoubleType => series.new_double_series(name, Array.empty[Double])
-      case BooleanType => series.new_boolean_series(name, Array.empty[Boolean])
-      case DateType => series.new_date_series(name, Array.empty[String])
-      case DateTimeType => series.new_datetime_series(name, Array.empty[String])
-      case StringType => series.new_str_series(name, Array.empty[String])
-      case t: DataType =>
-        throw new IllegalArgumentException(
-          s"Empty series of data type `${t.simpleName}` are currently not supported."
-        )
-    }
-
-    Series.withPtr(ptr)
-  }
+      }
+    )
 
   def withPtr(ptr: Long) = new Series(ptr)
 }
