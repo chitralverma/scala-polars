8443ae1c8a2811c54d7584d3c491406643c2f66d
Chitral Verma <chitralverma@gmail.com>
2024-03-30 18:31:11 +0530

    fmt fix

--- examples/src/main/scala/examples/scala/InstantiateDataFrame.scala
+++ examples/src/main/scala/examples/scala/InstantiateDataFrame.scala
@@ -5,17 +5,17 @@ import org.polars.scala.polars.api.{DataFrame, Series}
 object InstantiateDataFrame {
 
   def main(args: Array[String]): Unit = {
-    DataFrame.fromSeries(Series.ofBoolean("bool_col", Array[Boolean](true, false, true)),).show()
+    DataFrame.fromSeries(Series.ofBoolean("bool_col", Array[Boolean](true, false, true))).show()
 
     DataFrame
       .fromSeries(
         Series.ofInt("i32_col", Array[Int](1, 2, 3)),
-          Series.ofLong("i64_col", Array[Long](1L, 2L, 3L)),
-          Series.ofBoolean("bool_col", Array[Boolean](true, false, true)),
-          Series.ofList(
-            "nested_str_col",
-            Array[Array[String]](Array("a", "b", "c"), Array("a", "b", "c"), Array("a", "b", "c"))
-          )
+        Series.ofLong("i64_col", Array[Long](1L, 2L, 3L)),
+        Series.ofBoolean("bool_col", Array[Boolean](true, false, true)),
+        Series.ofList(
+          "nested_str_col",
+          Array[Array[String]](Array("a", "b", "c"), Array("a", "b", "c"), Array("a", "b", "c"))
+        )
       )
       .show()
 
