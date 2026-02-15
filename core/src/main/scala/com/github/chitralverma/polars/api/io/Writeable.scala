f47f9c5ff14b1d2d6929cf571c7b56d990ba3899
chitralverma <chitralverma@gmail.com>
2026-02-15 17:19:21 +0530

    doc change

--- core/src/main/scala/com/github/chitralverma/polars/api/io/Writeable.scala
+++ core/src/main/scala/com/github/chitralverma/polars/api/io/Writeable.scala
@@ -86,6 +86,8 @@ class Writeable private[polars] (ptr: Long) {
     *     Default: newest.
     *   - `write_compression`: Sets the compression codec used for pages. Supported values
     *     'uncompressed', 'lz4', 'zstd'. Default: zstd.
+    *   - `write_compression_level`: Sets a valid level for codecs like 'zstd'.
+    *     Defaults to compression default.
     *
     * @param filePath
     *   output file location
